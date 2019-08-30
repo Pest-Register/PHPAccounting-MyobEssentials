baad3f579c979ad1325d4ce8cfd2e583845bf649
Max Yendall <yendallmax@gmail.com>
2019-08-30 11:10:55 +1000

    Invoice Changes

--- src/Message/Invoices/Requests/CreateInvoiceRequest.php
+++ src/Message/Invoices/Requests/CreateInvoiceRequest.php
@@ -4,6 +4,7 @@ namespace PHPAccounting\MyobEssentials\Message\Invoices\Requests;
 
 use PHPAccounting\MyobEssentials\Helpers\IndexSanityInsertionHelper;
 use PHPAccounting\MyobEssentials\Message\AbstractRequest;
+use PHPAccounting\MyobEssentials\Message\InventoryItems\Responses\CreateInventoryItemResponse;
 use PHPAccounting\MyobEssentials\Message\Invoices\Responses\CreateInvoiceResponse;
 
 /**
@@ -12,6 +13,100 @@ use PHPAccounting\MyobEssentials\Message\Invoices\Responses\CreateInvoiceRespons
  */
 class CreateInvoiceRequest extends AbstractRequest
 {
+
+    /**
+     * Get Type Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return mixed
+     */
+    public function getType(){
+        return $this->getParameter('type');
+    }
+
+    /**
+     * Set Type Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @param string $value Invoice Type
+     * @return CreateInvoiceRequest
+     */
+    public function setType($value){
+        return $this->setParameter('type', $value);
+    }
+
+    /**
+     * Get Invoice Data Parameter from Parameter Bag (LineItems generic interface)
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return mixed
+     */
+    public function getInvoiceData(){
+        return $this->getParameter('invoice_data');
+    }
+
+    /**
+     * Set Invoice Data Parameter from Parameter Bag (LineItems)
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @param array $value Invoice Item Lines
+     * @return CreateInvoiceRequest
+     */
+    public function setInvoiceData($value){
+        return $this->setParameter('invoice_data', $value);
+    }
+
+    /**
+     * Get Date Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return mixed
+     */
+    public function getDate(){
+        return $this->getParameter('date');
+    }
+
+    /**
+     * Set Date Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @param string $value Invoice date
+     * @return CreateInvoiceRequest
+     */
+    public function setDate($value){
+        return $this->setParameter('date', $value);
+    }
+
+    /**
+     * Get Due Date Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return mixed
+     */
+    public function getDueDate(){
+        return $this->getParameter('due_date');
+    }
+
+    /**
+     * Set Due Date Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @param string $value Invoice Due Date
+     * @return CreateInvoiceRequest
+     */
+    public function setDueDate($value){
+        return $this->setParameter('due_date', $value);
+    }
+
+    /**
+     * Get Contact Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return mixed
+     */
+    public function getContact(){
+        return $this->getParameter('contact');
+    }
+
+    /**
+     * Set Contact Parameter from Parameter Bag
+     * @see https://developer.myob.com/api/essentials-accounting/endpoints/sale/invoices
+     * @return CreateInvoiceRequest
+     */
+    public function setContact($value){
+        return $this->setParameter('contact', $value);
+    }
 
     /**
      * Get the raw data array for this message. The format of this varies from gateway to
@@ -25,26 +120,20 @@ class CreateInvoiceRequest extends AbstractRequest
         return $this->data;
     }
 
-    /**
-     * Send Data to Xero Endpoint and Retrieve Response via Response Interface
-     * @param mixed $data Parameter Bag Variables After Validation
-     * @return \Omnipay\Common\Message\ResponseInterface|CreateInvoiceResponse
-     */
-    public function sendData($data)
+    public function getEndpoint()
     {
-        $response = [];
-        return $this->createResponse($response->getElements());
+
+        $endpoint = 'sale/invoices';
+        return $endpoint;
     }
 
-    /**
-     * Create Generic Response from Xero Endpoint
-     * @param mixed $data Array Elements or Xero Collection from Response
-     * @return CreateInvoiceResponse
-     */
-    public function createResponse($data)
+    public function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createResponse($data, $headers = [])
     {
         return $this->response = new CreateInvoiceResponse($this, $data);
     }
-
-
 }
